abef550279b8e6eb6fa576bc82dbd056f154385a
Bruno Quint <bquint@ctio.noao.edu>
2017-11-22 00:47:59 -0300

    Updated Travis CI

--- bin/sami_reduce.py
+++ bin/sami_reduce.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python 
 # -*- coding: utf8 -*-
 
-__author__ = 'Bruno Quint'
-
-
 import soar_sami
 
-test_path = '/home/bquint/Data/SAM/20160927/RAW' # soarbr3
+__author__ = 'Bruno Quint'
+
+# test_path = '/home/bquint/Data/SAM/20160927/RAW' # soarbr3
 test_path = '/Users/Bruno/Data/20170402/'
 
 soar_sami.classifier.classify(test_path)
