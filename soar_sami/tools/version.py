3c8478169132da544bb3874b4d35e01f435cfa3f
Bruno Quint <b1quint@gmail.com>
2018-06-16 02:30:48 -0700

    Updated version

--- soar_sami/tools/version.py
+++ soar_sami/tools/version.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 v0.0.0 - Initial Version.
+v0.1.0 - Pipeline actually running.
+       - To Do: Improve LaCosmic parameters.
 """
 
 api = 0
-feature = 0
+feature = 1
 bug = 0
 
-month = 12
-year = 2017
+month = 06
+year = 2018
 
 __str__ = "v{api:d}.{feature:d}.{bug:d} {month:d}, {year:d}".format(**locals())
