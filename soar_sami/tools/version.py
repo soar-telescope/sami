8fe2366973124533594f0796f955a1651f9f52f5
Bruno Quint <b1quint@gmail.com>
2018-06-22 14:45:42 -0400

    Updated version and history

--- soar_sami/tools/version.py
+++ soar_sami/tools/version.py
@@ -5,11 +5,12 @@ v0.1.0 - Pipeline actually running.
        - To Do: Improve LaCosmic parameters.
 v0.1.1 - Fixed bug related to master bias and flat names.
        - Added prefix to reduced data.
+v0.1.2 - Fixed bug related to the logging system.
 """
 
 api = 0
 feature = 1
-bug = 1
+bug = 2
 
 month = 6
 year = 2018
