c9adfaf49a8e68900e16c87d6493e42ac95a620d
Bruno Quint <b1quint@gmail.com>
2018-06-22 15:21:46 -0400

    Updated required files.

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='soar pipelines astronomy images spectroscopy',
+    keywords='soar pipelines astronomy images',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
@@ -83,7 +83,12 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    #install_requires=[''],
+    install_requires=[
+        'astropy',
+        'ccdproc',
+        'numpy',
+        'scipy'
+    ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
